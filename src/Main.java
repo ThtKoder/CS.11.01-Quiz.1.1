169c1318d39213dc81c655be9667057482a97a32
sander <sanderd2026@student.cis.edu.hk>
2023-09-13 09:34:43 +0800

    I found this lab to be not too difficult, but I had to be mindful not to make careless mistakes.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
 
+        System.out.println("Please enter your age followed by the return key:");
+        age = scanner.nextInt();
 
+        System.out.println("Please enter your First Name followed by the return key:");
+        firstName = scanner.next();
 
+        System.out.println("Please enter your Favourite Food followed by the return key:");
+        favouriteFood = scanner.next();
+
+        System.out.println("\nFirst Name: " + firstName + "\n\n" + "Age " + age + "\n\n" + "Favourite Food: " + favouriteFood);
+
+        //For next time: would like to be able to clear screen as well
 
     }
 
